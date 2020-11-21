7d5fc00b72f085ab2a6a71a36e138da51a9f0606
pinkcigareti <megser0625@gmail.com>
2020-11-21 18:23:42 +0300

    Support for sending files as a photo

--- src/bot.py
+++ src/bot.py
@@ -11,17 +11,49 @@ def handle_start(message):
     bot.send_message(message.chat.id, "Waifu2x Telegram Bot \n \n How to use: send photo")
     pass
 
+
 @bot.message_handler(commands=['help'])
 def handle_help(message):
     bot.send_message(message.chat.id, "Waifu2x Telegram Bot \n \n How to use: send photo")
-@bot.message_handler(content_types=['document'])
-def handle_docs_photo(message):
-    try:
 
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    try:
+        photo_info = bot.get_file(message.photo[0].file_id)
+        downloaded_file = bot.download_file(photo_info.file_path)
+        src = 'Your path to output image' + message.photo[0].file_id
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+            r = requests.post(
+                "https://api.deepai.org/api/waifu2x",
+                files={
+                    'image': downloaded_file,
+                },
+                headers={'api-key': 'YOUR_API_KEY'}
+            )
+            data1 = json.dumps(r.json())
+            data2 = json.loads(data1)
+        bot.reply_to(message, "Please wait")
+        url = data2['output_url']
+        f = open('output.png', 'wb')
+        f.write(urllib.request.urlopen(url).read())
+        f.close()
+        photo = open('output.png', 'rb')
+
+        bot.send_photo(message.chat.id, photo)
+        bot.send_message(message.chat.id, "Done. Enjoy")
+
+    except Exception as e:
+        bot.reply_to(message, e)
+
+
+@bot.message_handler(content_types=['document'])
+def handle_docs(message):
+    try:
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-
-        src = 'Your Path to Output image' + message.document.file_name
+        src = 'Your path to output image' + message.document.file_name
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
             r = requests.post(
@@ -42,8 +74,10 @@ def handle_docs_photo(message):
 
         bot.send_document(message.chat.id, doc)
         bot.send_message(message.chat.id, "Done. Enjoy")
+
     except Exception as e:
         bot.reply_to(message, e)
 
 
 bot.polling()
+
